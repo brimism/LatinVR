bdeec1493908619a36a27b0bebce1e61d728c641
flamegiraffe <flamegiraffes@gmail.com>
2019-01-31 09:52:45 -0800

    jug giving start

---
+++ Assets/02_Scripts/CenturionScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenturionScript : MonoBehaviour {
+	public AudioClip giveJug;
+	public AudioClip goodJob;
+
+	private AudioSource aus;
+	void Start () {
+		aus = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter(Collider c){
+		if(c.gameObject.name == "Jug"){
+			aus.clip = goodJob;
+			aus.Play();
+		}else if(c.transform.root.tag == "Player"){
+
+		}else{
+			aus.clip = giveJug;
+			aus.Play();
+		}
+	}
+}
