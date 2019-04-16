6133aed627e9677286505d3333571ff3db7f766d
flamegiraffe <flamegiraffes@gmail.com>
2019-04-16 14:38:49 -0700

    mv state machine added

--- Assets/02_Scripts/GameManagerScript.cs
+++ Assets/02_Scripts/GameManagerScript.cs
@@ -7,6 +7,7 @@ public class GameManagerScript : MonoBehaviour {
 	public bool giveJug = false;
 	public bool pointToBox = false;
 	public bool pointToWheel = false;
+	public bool oneSecElapsed = false;
 	public int currHead = 0;
 	public TextScript textBubble;
 
@@ -18,26 +19,41 @@ public class GameManagerScript : MonoBehaviour {
 
 	[System.Serializable]
 	public class Node{
-		public string text;
+		public bool isVocabMatchingParent = false;
+		public bool isVocabMatchingSuccess = false;
+		public bool isVocabMatchingFail = false;
+		public int lastSelected;
+		public string text = "";
+		public AudioClip ac;
 		public List<Edge> edges = new List<Edge>();
 	}
 
 	public List<Node> graph = new List<Node>();
 	Dictionary<string, bool> boolDict = new Dictionary<string, bool>();
+	private AudioSource aus;
+	private Timer timer;
 
 	public void Start(){
-		UpdateDict();
+		aus = GetComponent<AudioSource>();
+		timer = GameObject.Find("Timer").GetComponent<Timer>();
+		SetupDict();
 		Reevaluate();
+		textBubble.ChangeText(graph[currHead].text);
+		if(graph[currHead].ac!=null){
+			aus.clip = graph[currHead].ac;
+			aus.Play();
+		}
 	}
 
 	public void UpdateValue(string s, bool b){
 		boolDict[s] = b;
 	}
 
-	public void UpdateDict(){
+	public void SetupDict(){
 		boolDict["give jug"] = giveJug;
 		boolDict["point to box"] = pointToBox;
 		boolDict["point to wheel"] = pointToWheel;
+		boolDict["1s"] = oneSecElapsed;
 	}
 
 	public void Reevaluate(){
@@ -46,10 +62,29 @@ public class GameManagerScript : MonoBehaviour {
 		for(int i=0; i<n.edges.Count; i++){
 			if(boolDict[n.edges[i].condition.ToLower()]){
 				currHead = n.edges[i].toNode;
+				if(graph[currHead].isVocabMatchingParent){
+					if(graph[currHead].edges.Count>1){
+						graph[currHead].lastSelected = Random.Range(0, graph[currHead].edges.Count-2);
+						currHead = graph[currHead].edges[graph[currHead].lastSelected].toNode;
+					}else{
+						currHead = graph[currHead].edges[0].toNode;
+					}
+				}
+				if(graph[currHead].isVocabMatchingSuccess){
+					Node vmp = graph[graph[currHead].edges[0].toNode];
+					vmp.edges.RemoveAt(vmp.lastSelected);
+					timer.StartTimer();
+				}else if(graph[currHead].isVocabMatchingFail){
+					timer.StartTimer();
+				}
+				textBubble.ChangeText(graph[currHead].text);
+				if(graph[currHead].ac!=null){
+					aus.clip = graph[currHead].ac;
+					aus.Play();
+				}
 				break;
 			}
 		}
-		textBubble.ChangeText(graph[currHead].text);
 	}
 
 	void Update () {
---
+++ Assets/02_Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	private float startTime = 0f;
+	private bool counting = false;
+	private GameManagerScript gm;
+
+	void Start(){
+		gm = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+	}
+	void Update () {
+		if(counting){
+			if(Time.time>startTime+1f){
+				counting = false;
+				gm.UpdateValue("1s", true);
+				gm.Reevaluate();
+				gm.UpdateValue("1s", false);
+			}
+		}
+	}
+
+	public void StartTimer(){
+		counting = true;
+		startTime = Time.time;
+	}
+}
