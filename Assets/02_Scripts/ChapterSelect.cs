f926545ec0345d64929debfb366db128cb67087d
Brian Beams <brimism@gmail.com>
2019-10-15 11:09:39 -0700

    Menu layout and word sorting test
    Menu layout and word sorting test

---
+++ Assets/02_Scripts/ChapterSelect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChapterSelect : MonoBehaviour {
+    int[] chapterSceneIndex;
+    public GameObject[] chapterCards = new GameObject[12];
+    public GameObject[] chapterButtons = new GameObject[12];
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadScene(int chapterNum)
+    {
+        SceneManager.LoadScene(chapterSceneIndex[chapterNum]);
+    }
+}
