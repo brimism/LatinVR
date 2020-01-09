93a0d30e41f3f3e56990e22cf3b15e0d4214ec02
Mohammed Khadadeh <mkhadadeh@scu.edu>
2020-01-08 20:41:52 -0800

    <wave> tag implemented but buggy

--- Assets/00_LinguaVitaeAssets/010_Scripts/Dialogue Extensions/Custom Tags/shake_TMPTag.cs
+++ Assets/00_LinguaVitaeAssets/010_Scripts/Dialogue Extensions/Custom Tags/shake_TMPTag.cs
@@ -36,13 +36,6 @@ public class shake_TMPTag : CustomTMPTag
         var textInfo = text.textInfo;
         Matrix4x4 transformMatrix;
 
-        VertexAnim[] vertexAnim = new VertexAnim[1024];
-        for (int i = 0; i < 1024; i++)
-        {
-            vertexAnim[i].angleRange = Random.Range(10f, 25f);
-            vertexAnim[i].speed = Random.Range(1f, 3f);
-        }
-
         // Cache the vertex data of the text object as the Jitter FX is applied to the original position of the characters.
         TMPro.TMP_MeshInfo[] cachedMeshInfo = textInfo.CopyMeshInfoVertexData();
 
@@ -66,9 +59,6 @@ public class shake_TMPTag : CustomTMPTag
                 if (!charInfo.isVisible)
                     continue;
 
-                // Retrieve the pre-computed animation data for the given character.
-                VertexAnim vertAnim = vertexAnim[i];
-
                 // Get the index of the material used by the current character.
                 int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
 
@@ -78,8 +68,6 @@ public class shake_TMPTag : CustomTMPTag
                 // Get the cached vertices of the mesh used by this text element (character or sprite).
                 Vector3[] sourceVertices = cachedMeshInfo[materialIndex].vertices;
 
-                // Determine the center point of each character at the baseline.
-                //Vector2 charMidBasline = new Vector2((sourceVertices[vertexIndex + 0].x + sourceVertices[vertexIndex + 2].x) / 2, charInfo.baseLine);
                 // Determine the center point of each character.
                 Vector2 charMidBasline = (sourceVertices[vertexIndex + 0] + sourceVertices[vertexIndex + 2]) / 2;
 
@@ -109,8 +97,6 @@ public class shake_TMPTag : CustomTMPTag
                 destinationVertices[vertexIndex + 1] += offset;
                 destinationVertices[vertexIndex + 2] += offset;
                 destinationVertices[vertexIndex + 3] += offset;
-
-                vertexAnim[i] = vertAnim;
             }
             // Push changes into meshes
             for (int i = 0; i < textInfo.meshInfo.Length; i++)
--- Assets/00_LinguaVitaeAssets/010_Scripts/VRCommands/NextDialogue2_CMD.cs
+++ Assets/00_LinguaVitaeAssets/010_Scripts/VRCommands/NextDialogue2_CMD.cs
@@ -6,16 +6,22 @@ using UnityEngine.XR;
 public class NextDialogue2_CMD : VRCommand
 {
     public TMPDialogueUI ui;
+    bool pressed = false;
     public override void RunCommand(TestResult tr, Bone val_bone = default, bool val_bool = false, Eyes val_eyes = default, float val_float = 0, Hand val_hand = default, InputTrackingState val_input_tracking_state = InputTrackingState.None, Quaternion val_quaternion = default, Vector2 val_vector2 = default, Vector3 val_vector3 = default)
     {
-        if (tr == TestResult.TRUE)
+        if (tr == TestResult.TRUE && !pressed)
         {
             Debug.Log("VR Input 2 detected");
             ui.VRInputDetected2 = true;
+            pressed = true;
         }
         else
         {
             ui.VRInputDetected2 = false;
+        }
+        if(tr == TestResult.FALSE)
+        {
+            pressed = false;
         }
     }
 
