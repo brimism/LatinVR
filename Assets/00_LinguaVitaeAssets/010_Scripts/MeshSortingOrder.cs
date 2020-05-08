414a0b183fa4fc5cdca690ddffbfea48df56b5d4
mkhadadeh <mkhadadeh@scu.edu>
2020-05-08 04:50:20 -0700

    Fixed Everything.

---
+++ Assets/00_LinguaVitaeAssets/010_Scripts/MeshSortingOrder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSortingOrder : MonoBehaviour
+{
+    public string layerName;
+    public int order;
+
+    private MeshRenderer rend;
+    void Awake()
+    {
+        rend = GetComponent<MeshRenderer>();
+        rend.sortingLayerName = layerName;
+        rend.sortingOrder = order;
+    }
+
+    public void Update()
+    {
+        if (rend.sortingLayerName != layerName)
+            rend.sortingLayerName = layerName;
+        if (rend.sortingOrder != order)
+            rend.sortingOrder = order;
+    }
+
+    public void OnValidate()
+    {
+        rend = GetComponent<MeshRenderer>();
+        rend.sortingLayerName = layerName;
+        rend.sortingOrder = order;
+    }
+}
